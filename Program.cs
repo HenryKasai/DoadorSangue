3c9839a370b0bbabb76632775f6642e4987d1440
HenryKasai <henrykasai1775@gmail.com>
2020-06-10 18:20:05 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DoadorSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Por favor, insira sua idade: ");
+            bool idadebool = Int32.TryParse(Console.ReadLine(), out idade);
+            if (!idadebool)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Valor inválido.");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Environment.Exit(-1);
+            }
+            else 
+            {
+                if(idade > 67 || idade < 18)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Você não pode ser um doador de sangue.");
+                }
+                else if (idade >= 18 || idade <= 67)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Você pode ser um doador de sangue.");
+                }
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            Console.ReadKey();
+        }
+    }
+}
